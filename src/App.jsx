fae94b0b29e61a974a4b710ed8d3b96f4513b48d
Nazareno Luna <nazarenoluna86@gmail.com>
2026-09-05 13:13:45 -0300

    feat: dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,49 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import MainLayout from "./layouts/MainLayout/MainLayout";
+
 import Login from "./pages/Login/Login";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
 import Clientes from "./pages/Clientes/Clientes";
 import CategoriasCliente from "./pages/CategoriasCliente/CategoriasCliente";
-import "./styles/forms.css";
+
 import Productos from "./pages/Productos/Productos";
+
 import Precios from "./pages/Precios/Precios";
 import NuevaListaPrecio from "./pages/Precios/NuevaListaPrecio";
 import ListaPrecioDetalle from "./pages/Precios/ListaPrecioDetalle";
+
 import Pedidos from "./pages/Pedidos/Pedidos";
 import PedidoForm from "./pages/Pedidos/PedidoForm";
+
 import PedidosHabituales from "./pages/PedidosHabituales/PedidosHabituales";
+
 import Rutas from "./pages/Rutas/Rutas";
 import RutaForm from "./pages/Rutas/RutaForm";
 import RutaDetalle from "./pages/Rutas/RutaDetalle";
+
 import Pagos from "./pages/Pagos/Pagos";
-import PlanillaVentas from "./pages/PlanillaVentas/PlanillaVentas";
-import Usuarios from "./pages/Usuarios/Usuarios";
+
 import Vehiculos from "./pages/Vehiculos/Vehiculos";
+
 import Remitos from "./pages/Remitos/Remitos";
 import RemitoNuevo from "./pages/Remitos/RemitoNuevo";
 import RemitoDetalle from "./pages/Remitos/RemitoDetalle";
+
+import Usuarios from "./pages/Usuarios/Usuarios";
+
 import Cheques from "./pages/Cheques/Cheques";
+
 import Gastos from "./pages/Gastos/Gastos";
 import Perdidas from "./pages/Perdidas/Perdidas";
-import Croquis from "./pages/Croquis/Croquis";
+
 import ControlHorario from "./pages/ControlHorario/ControlHorario";
 
-//sadasd
+import PlanillaVentas from "./pages/PlanillaVentas/PlanillaVentas";
+
+import Croquis from "./pages/Croquis/Croquis";
+
 function App() {
   return (
     <BrowserRouter>
@@ -44,16 +59,9 @@ function App() {
               </ProtectedRoute>
             }
           >
-            <Route path="/" element={<div>Dashboard (placeholder)</div>} />
+            <Route path="/" element={<Dashboard />} />
+
             <Route path="/clientes" element={<Clientes />} />
-            <Route
-              path="/usuarios"
-              element={
-                <ProtectedRoute rolesPermitidos={["ADMIN"]}>
-                  <Usuarios />
-                </ProtectedRoute>
-              }
-            />
             <Route
               path="/categorias-clientes"
               element={
@@ -62,6 +70,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
+
             <Route path="/productos" element={<Productos />} />
 
             <Route
@@ -88,10 +97,13 @@ function App() {
                 </ProtectedRoute>
               }
             />
+
             <Route path="/pedidos" element={<Pedidos />} />
             <Route path="/pedidos/nuevo" element={<PedidoForm />} />
             <Route path="/pedidos/:id/editar" element={<PedidoForm />} />
+
             <Route path="/pedidos-habituales" element={<PedidosHabituales />} />
+
             <Route path="/rutas" element={<Rutas />} />
             <Route path="/rutas/:id" element={<RutaDetalle />} />
             <Route
@@ -110,12 +122,24 @@ function App() {
                 </ProtectedRoute>
               }
             />
+
             <Route path="/pagos" element={<Pagos />} />
-            <Route path="/planilla-ventas" element={<PlanillaVentas />} />
+
             <Route path="/vehiculos" element={<Vehiculos />} />
+
             <Route path="/remitos" element={<Remitos />} />
             <Route path="/remitos/nuevo" element={<RemitoNuevo />} />
             <Route path="/remitos/:id" element={<RemitoDetalle />} />
+
+            <Route
+              path="/usuarios"
+              element={
+                <ProtectedRoute rolesPermitidos={["ADMIN"]}>
+                  <Usuarios />
+                </ProtectedRoute>
+              }
+            />
+
             <Route
               path="/cheques"
               element={
@@ -124,11 +148,15 @@ function App() {
                 </ProtectedRoute>
               }
             />
+
             <Route path="/gastos" element={<Gastos />} />
             <Route path="/perdidas" element={<Perdidas />} />
-            <Route path="/croquis" element={<Croquis />} />
+
             <Route path="/control-horario" element={<ControlHorario />} />
-        {/* ----------------------------------------------------------------------------------- */}
+
+            <Route path="/planilla-ventas" element={<PlanillaVentas />} />
+
+            <Route path="/croquis" element={<Croquis />} />
           </Route>
         </Routes>
       </AuthProvider>
