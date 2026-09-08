b3c9742b026ef1a8da4580fa3e2001f4d0378e5f
Nazareno Luna <nazarenoluna86@gmail.com>
2026-09-08 19:17:09 -0300

    refactor: sidebar para celulares (se oculta en pantallas chicas)

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Navbar from "../../components/Navbar/Navbar";
 import "./MainLayout.css";
@@ -10,16 +10,36 @@ export default function MainLayout() {
   const [colapsado, setColapsado] = useState(() => {
     return localStorage.getItem(STORAGE_KEY) === "true";
   });
+  const [abiertoMobile, setAbiertoMobile] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, String(colapsado));
   }, [colapsado]);
 
+  // Cierra el drawer mobile automaticamente al cambiar de pagina
+  useEffect(() => {
+    setAbiertoMobile(false);
+  }, [location.pathname]);
+
   return (
     <div className="main-layout">
-      <Sidebar colapsado={colapsado} onToggle={() => setColapsado((c) => !c)} />
+      <Sidebar
+        colapsado={colapsado}
+        onToggle={() => setColapsado((c) => !c)}
+        abiertoMobile={abiertoMobile}
+        onCerrarMobile={() => setAbiertoMobile(false)}
+      />
+
+      {abiertoMobile && (
+        <div
+          className="sidebar-overlay"
+          onClick={() => setAbiertoMobile(false)}
+        />
+      )}
+
       <div className="main-content-wrapper">
-        <Navbar />
+        <Navbar onAbrirMenu={() => setAbiertoMobile(true)} />
         <main className="main-content">
           <Outlet />
         </main>
