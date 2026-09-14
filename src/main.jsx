9f0bd1dc54ef0aceb0f22d980c268d49e5f41418
valentino <vlmvalenlozanomassey@gmail.com>
2026-09-13 23:18:22 -0300

    mejora estetica de los <modal> de los formularios

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./styles/variables.css";
 import './index.css'
 import App from './App.jsx'
 import "./styles/common.css";
+import "./styles/forms.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
